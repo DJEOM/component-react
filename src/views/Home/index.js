b2cd15b30e97ac896a822c580db53b0819f2664e
wush <97171834+DJEOM@users.noreply.github.com>
2024-03-13 10:10:08 +0800

    📝 update：explanatory note

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../logo.svg";
+// import logo from "../../logo.svg";
 import "../../App.css";
 // import { Link, useNavigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h2>React状态管理专题：深入探讨组件组合——Component Composition</h2>
         <p>有以下几种方式实现状态管理:</p>
         {/* 三种跳转方式 */}
