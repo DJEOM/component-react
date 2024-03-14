3cd915aec9e74e5d0c38c42671ea1b4a3724921c
wush <97171834+DJEOM@users.noreply.github.com>
2024-03-14 13:43:33 +0800

    ⚡️ update：优化路由组件导入方式

--- src/router/index.js
+++ src/router/index.js
@@ -6,39 +6,39 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 /* 
 路由懒加载——使用Suspense占位符
 */
-let Home = React.lazy(() => import("../views/Home"));
-let UseProps = React.lazy(() => import("../components/use-props"));
-let Children = React.lazy(() => import("../components/use-children"));
-
+// let Home = React.lazy(() => import("../views/Home"));
+// let UseProps = React.lazy(() => import("../components/use-props"));
+// let Children = React.lazy(() => import("../components/use-children"));
+let routesPath = [
+  {
+    path: "/",
+    component: React.lazy(() => import("../views/Home")),
+  },
+  {
+    path: "/use/props/:num",
+    component: React.lazy(() => import("../components/use-props")),
+  },
+  {
+    path: "/use/children/:num",
+    component: React.lazy(() => import("../components/use-children")),
+  },
+];
 export default function RouterView() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home></Home>}></Route> */}
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<div>loading</div>}>
-              <Home />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/use/props/:num"
-          element={
-            <Suspense fallback={<div>loading</div>}>
-              <UseProps />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/use/children/:num"
-          element={
-            <Suspense fallback={<div>loading</div>}>
-              <Children />
-            </Suspense>
-          }
-        ></Route>
+        {routesPath.map((item) => {
+          return (
+            <Route
+              path={item.path}
+              element={
+                <Suspense fallback={<div>loading</div>}>
+                  {<item.component />}
+                </Suspense>
+              }
+            ></Route>
+          );
+        })}
       </Routes>
     </Router>
   );
